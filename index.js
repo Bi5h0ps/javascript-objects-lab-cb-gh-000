dcf84fb4c48e0f861f4b767e3004d74c23c566e0
Haoyuan Guo <h62guo@uwaterloo.ca>
2018-02-03 18:56:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
   console.log(object);
-  var new1 = Object.assign(objecct,{[key]:value});
-  console.log(object);
+  var new1 = Object.assign({},objecct);
+  var new1 = Object.assign(objecct,{[rinima]:"haha"});
   console.log(new1);
+  console.log(new2);
   return Object.assign({},object,{[key]:value});
 }
 
