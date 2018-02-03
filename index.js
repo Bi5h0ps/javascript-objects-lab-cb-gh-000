c9f873a42f095d6ee2f4645b7fda7fcdc69e8daf
Haoyuan Guo <h62guo@uwaterloo.ca>
2018-02-03 18:51:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 var recipes = new Object();
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object,{[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var toreturn = Object.assign({},object);
+  delete toreturn[key];
+  return toreturn;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
